536ec5958d10f175a6c15342b9670805a96c4de5
anette87 <anettem.rivera@gmail.com>
2020-06-11 16:48:04 -0700

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,20 @@
-class Model
+#Models represent the data and object logic of our application.
+
+class Dog
+
+    attr_accessor :name, :breed, :age 
+
+    @@all = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed 
+        @age = age
+        @@all << self
+    end 
+
+    def self.all
+        @@all 
+    end 
 
 end
